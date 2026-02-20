fda4259722a93087aa1842290d938ef34e5dbf98
ajamol2197 <ajamol2197@gmail.com>
2026-02-20 16:26:22 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,68 @@
+#1
+def a(name):
+    print(f"salom {name}")
+
+
+a("ali")
+
+# 2
+def a(yosh):
+    print(yosh)
+
+
+a(12)
+
+# 3
+def a(i):
+    print(a)
+
+
+print(1+2)
+
+# 4
+def i(a, b):
+    print(f"{a} {b} yoshda")
+
+
+i("bunyod", 15)
+
+# 5
+def a(b):
+    print(b*b)
+
+
+a(2)
+
+# 6
+def tak(b,i):
+    print(b*i)
+
+
+tak(12, 4)
+
+# 7
+def raq(a, b):
+    print(a - b)
+
+raq(12, 4)
+
+# 8
+def ism(a):
+    print(f"xurmatli {a}")
+
+
+ism("ali")
+
+# 9
+def kop(a, b):
+    print(a*b)
+
+
+kop(2, 2)
+
+# 10
+def bol(a, b):
+    print(a/b)
+
+
+bol(2, 2)
